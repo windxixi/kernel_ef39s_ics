01f6c181be3861a705d8cf522c8963a5c61d644b
windxixi <windxixi@gmail.com>
2013-01-02 18:48:39 +0800

    maybe fix mdp loop reboot

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 28 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#28 SMP PREEMPT Sat Dec 8 23:08:10 CST 2012"
+#define UTS_VERSION "#28 SMP PREEMPT Wed Jan 2 18:24:38 CST 2013"
 #define LINUX_COMPILE_BY "windxixi"
-#define LINUX_COMPILE_HOST "Winsrv"
+#define LINUX_COMPILE_HOST "WinSrv"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.8-perf-gccae54f-dirty"
+#define UTS_RELEASE "3.0.8-perf-g7410745-dirty"
