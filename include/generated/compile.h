c7dd05f0c9d048dc70af276be73d289a1b342cb0
windxixi <windxixi@gmail.com>
2013-01-02 22:29:23 +0800

    add more cpu-governors

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 28 */
+/* This file is auto generated, version 30 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#28 SMP PREEMPT Wed Jan 2 18:24:38 CST 2013"
+#define UTS_VERSION "#30 SMP PREEMPT Wed Jan 2 22:22:18 CST 2013"
 #define LINUX_COMPILE_BY "windxixi"
 #define LINUX_COMPILE_HOST "WinSrv"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.8-perf-g7410745-dirty"
+#define UTS_RELEASE "3.0.8-perf-g01f6c18-dirty"
